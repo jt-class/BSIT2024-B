ccfc6db092a642882a22ed282533f3c5d8758bfb
chardgrey <cardosarichard@gmail.com>
2024-09-10 02:05:55 +0800

    About
    added about

--- menu-scene/scripts/main_menu.gd
+++ menu-scene/scripts/main_menu.gd
@@ -4,3 +4,7 @@ extends Control
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://BSIT2024-B/menu-scene/options_menu.tscn")
+
+
+func _on_about_pressed() -> void:
+	get_tree().change_scene_to_file("res://BSIT2024-B/menu-scene/About.tscn")
